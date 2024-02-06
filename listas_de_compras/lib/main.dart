b0960ced2d7cbc1d3869cad85e77fb52b7df2cad
Mariano Salvetti <mariano_salvetti@gap.com>
2024-02-06 11:02:22 -0300

    Add state management and dropdown menu

--- listas_de_compras/lib/main.dart
+++ listas_de_compras/lib/main.dart
@@ -1,34 +1,94 @@
 import 'package:flutter/material.dart';
-
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'state_managment.dart';
 void main() {
-  runApp(MainApp());
+  runApp(const ProviderScope(child: MyApp()));
 }
 
-class MainApp extends StatelessWidget {
-  MainApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
 
-  final List<String> data = <String>['Hello', 'Flutter', 'World'];
-  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body: ListView.builder(
-          itemCount: data.length,
-          itemBuilder: (BuildContext context, int index) {
-            return buildCard(data[index]);
-          },
-        ),
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.red,
       ),
+      home: const MyHomePage(),
     );
   }
+}
 
-  Widget buildCard(String text) {
-    return Card(
-      child: Padding(
+class MyHomePage extends ConsumerWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    List<CartItem> cartItems = ref.watch(filteredCartListProvider);
+
+    // Conteo total de items para añadir uno nuevo a partir del último
+    int totalItems = ref.read(cartListProvider.notifier).countTotalItems();
+
+    // Valor del menu dropDown
+    String dropDownValue = ref.watch(menuProvider);
+
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: false,
+        title: const Text('Lista del super'),
+        backgroundColor: Colors.red,
+        actions: [
+          DropdownButton(
+              dropdownColor: Colors.black,
+              style: const TextStyle(color: Colors.white),
+              icon: const Icon(
+                Icons.arrow_drop_down,
+                color: Colors.black,
+              ),
+              value: dropDownValue,
+
+              // se crea una lista de items a partir de la que definimos en state_managment.dart
+              items: menuItems
+                  .map((String e) => DropdownMenuItem(
+                        value: e,
+                        child: Text(e),
+                      ))
+                  .toList(),
+
+              // al cambiar se asigna un nuevo valor al provider del menu
+              onChanged: (String? value) => 
+               ref.read(menuProvider.notifier)
+                  .update((state) => state = value!))
+        ],
+      ),
+      body: Padding(
         padding: const EdgeInsets.all(8.0),
-        child: Text(text),
+        child: ListView.separated(
+            itemBuilder: ((context, index) {
+              return Card(
+                elevation: 2,
+                child: CheckboxListTile(
+                  title: Text('Item de compra ${cartItems[index].number}'),
+                  value: cartItems[index].inCart,
+                  onChanged: (value) => ref
+                      .read(cartListProvider.notifier)
+                      .toggle(cartItems[index].number),
+                      
+                ),
+              );
+            }),
+            separatorBuilder: ((context, index) => const SizedBox(
+                  height: 8,
+                )),
+            itemCount: cartItems.length),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add),
+        onPressed: () => ref
+            .read(cartListProvider.notifier)
+            .add(CartItem(number: totalItems, inCart: false)),
       ),
     );
   }
